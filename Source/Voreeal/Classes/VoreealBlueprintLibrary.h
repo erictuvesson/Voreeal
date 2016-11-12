6b750a9978591afafc9a0be5b387bca8c40db771
Eric Tuvesson <eric.tuvesson@gmail.com>
2016-11-12 20:55:47 +0100

    changes

--- Source/Voreeal/Classes/VoreealBlueprintLibrary.h
+++ Source/Voreeal/Classes/VoreealBlueprintLibrary.h
@@ -10,41 +10,41 @@ class UVoreealBlueprintLibrary : public UBlueprintFunctionLibrary
 
 public: // Region Methods
 
-	/// Gets the center of the region.
+	/*/// Gets the center of the region.
 	UFUNCTION(BlueprintCallable, BlueprintPure, Category = "Voreeal|Region")
-	static void GetCenter(const FVoreealRegion& Region, FVector& Result);
+	static void GetCenter(const FVoreealRegion& Region, FVector& Result) {
+		Result = Region.GetCenter();
+	}
 
 	/// Gets the upper corner of the region.
 	UFUNCTION(BlueprintCallable, BlueprintPure, Category = "Voreeal|Region")
-	static void GetUpperCorner(const FVoreealRegion& Region, FVector& Result);
+	static void GetUpperCorner(const FVoreealRegion& Region, FVector& Result) {
+		Result = Region.GetUpper();
+	}
 
 	/// Gets the lower corner of the region.
 	UFUNCTION(BlueprintCallable, BlueprintPure, Category = "Voreeal|Region")
-	static void GetLowerCorner(const FVoreealRegion& Region, FVector& Result);
-
-	/// Grow the region from every direction.
-	UFUNCTION(BlueprintCallable, BlueprintPure, Category = "Voreeal|Region")
-	static void GrowUnified(const FVoreealRegion& Region, int32 Amount, FVoreealRegion& Result) { Grow(Region, Amount, Amount, Amount, Result); }
+	static void GetLowerCorner(const FVoreealRegion& Region, FVector& Result) {
+		Result = Region.GetLower();
+	}
 
 	/// Grow the region.
 	UFUNCTION(BlueprintCallable, BlueprintPure, Category = "Voreeal|Region")
-	static void Grow(const FVoreealRegion& Region, int32 Width, int32 Height, int32 Depth, FVoreealRegion& Result);
+	static void Grow(const FVoreealRegion& Region, int32 Width, int32 Height, int32 Depth, FVoreealRegion& Result) {
+		Region.Grow(Width, Height, Depth, Result);
+	}
 
-	/// Shift the upper corner of the region.
+	/// Grow the region from every direction.
 	UFUNCTION(BlueprintCallable, BlueprintPure, Category = "Voreeal|Region")
-	static void ShiftUpperCorner(const FVoreealRegion& Region, int32 X, int32 Y, int32 Z, FVoreealRegion& Result);
+	static void GrowUnified(const FVoreealRegion& Region, int32 Amount, FVoreealRegion& Result) { 
+		Region.GrowUnified(Amount, Result);
+	}*/
 
-	/// Shift the lower corner of the region.
-	UFUNCTION(BlueprintCallable, BlueprintPure, Category = "Voreeal|Region")
-	static void ShiftLowerCorner(const FVoreealRegion& Region, int32 X, int32 Y, int32 Z, FVoreealRegion& Result);
+	//static void ShiftUpperCorner(const FVoreealRegion& Region, int32 X, int32 Y, int32 Z, FVoreealRegion& Result);
+	//static void ShiftLowerCorner(const FVoreealRegion& Region, int32 X, int32 Y, int32 Z, FVoreealRegion& Result);
 
-	/// Region intersect
-	UFUNCTION(BlueprintCallable, BlueprintPure, Category = "Voreeal|Region")
-	static bool Contains(const FVoreealRegion& Region1, const FVoreealRegion& Region2);
-
-	/// Region intersect
-	UFUNCTION(BlueprintCallable, BlueprintPure, Category = "Voreeal|Region")
-	static bool Intersect(const FVoreealRegion& Region1, const FVoreealRegion& Region2);
+	//static bool Contains(const FVoreealRegion& Region1, const FVoreealRegion& Region2);
+	//static bool Intersect(const FVoreealRegion& Region1, const FVoreealRegion& Region2);
 
 public:
 
--- Source/Voreeal/Classes/VoreealRegion.h
+++ Source/Voreeal/Classes/VoreealRegion.h
@@ -10,24 +10,31 @@ struct VOREEAL_API FVoreealRegion
 {
 	GENERATED_BODY()
 
+	// Position X component.
 	UPROPERTY(EditAnywhere, BlueprintReadWrite)
 	int32 X;
 
+	// Position Y component.
 	UPROPERTY(EditAnywhere, BlueprintReadWrite)
 	int32 Y;
 
+	// Position Z component.
 	UPROPERTY(EditAnywhere, BlueprintReadWrite)
 	int32 Z;
 
+	// Position Width component.
 	UPROPERTY(EditAnywhere, BlueprintReadWrite)
 	int32 Width;
 
+	// Position Height component.
 	UPROPERTY(EditAnywhere, BlueprintReadWrite)
 	int32 Height;
 
+	// Position Depth component.
 	UPROPERTY(EditAnywhere, BlueprintReadWrite)
 	int32 Depth;
 
+	// Default constructor
 	FORCEINLINE FVoreealRegion()
 		: X(0), Y(0), Z(0), Width(0), Height(0), Depth(0)
 	{ }
@@ -36,8 +43,8 @@ struct VOREEAL_API FVoreealRegion
 		: X(X), Y(Y), Z(Z), Width(Width), Height(Height), Depth(Depth)
 	{ }
 
-	explicit FORCEINLINE FVoreealRegion(FVector Upper)
-		: FVoreealRegion(FVector::ZeroVector, Upper)
+	explicit FORCEINLINE FVoreealRegion(FVector Size)
+		: FVoreealRegion(FVector::ZeroVector, Size)
 	{ }
 
 	FORCEINLINE FVoreealRegion(FVector Lower, FVector Upper)
@@ -50,23 +57,55 @@ struct VOREEAL_API FVoreealRegion
 		, Width(Lower.X - Upper.X), Height(Lower.Y - Upper.Y), Depth(Lower.Z - Upper.Z)
 	{ }
 
+	// Convert PolyVox::Region to FVoreealRegion.
 	FORCEINLINE FVoreealRegion(const PolyVox::Region &InRegion)
 	{
 		FVoreealRegion::operator=(InRegion);
 	}
  
-	/// Gets the upper corner.
+	// Gets the upper corner.
 	FVector Min() const { return FVector(X, Y, Z); }
 
-	/// Gets the lower corner.
+	// Gets the lower corner.
 	FVector Max() const { return FVector(X + Width, Y + Height, Z + Depth); }
 
-	FIntVector GetUpper() const { return FIntVector(X, Y, Z); }
-	FIntVector GetLower() const { return FIntVector(X + Width, Y + Height, Z + Depth); }
+	FIntVector GetUpperInt() const;
+	FIntVector GetLowerInt() const;
 
-	/// Gets the size.
+	FVector GetUpper() const; 
+	FVector GetLower() const;
+
+	// Gets the size.
 	FVector Size() const { return FVector(Width, Height, Depth); }
 
+	// Gets the center of the region.
+	FVector GetCenter() const;
+
+	// Grow the region.
+	void Grow(int32 Width, int32 Height, int32 Depth);
+
+	// Grow the region.
+	void Grow(int32 Width, int32 Height, int32 Depth, FVoreealRegion& Result);
+
+	// Grow the region from every direction.
+	void GrowUnified(int32 Amount);
+
+	// Grow the region from every direction.
+	void GrowUnified(int32 Amount, FVoreealRegion& Result);
+
+	// Shift the upper corner of the region.
+	void ShiftUpperCorner(int32 X, int32 Y, int32 Z);
+
+	// Shift the lower corner of the region.
+	void ShiftLowerCorner(int32 X, int32 Y, int32 Z);
+
+	// Does this region contain another region.
+	static bool Contains(const FVoreealRegion& Region1, const FVoreealRegion& Region2);
+
+	// Does this region intersect another region.
+	static bool Intersect(const FVoreealRegion& Region1, const FVoreealRegion& Region2);
+
+	// Region to string.
 	FORCEINLINE FString ToString() const
 	{
 		return FString::Printf(TEXT("Min=[%s] Max=[%s]"), *Min().ToString(), *Max().ToString());
@@ -100,3 +139,108 @@ struct VOREEAL_API FVoreealRegion
 		return Ar;
 	}
 };
+
+FORCEINLINE FVector FVoreealRegion::GetUpper() const
+{
+	return FVector(X, Y, Z);
+}
+
+FORCEINLINE FVector FVoreealRegion::GetLower() const
+{
+	return FVector(X + Width, Y + Height, Z + Depth);
+}
+
+FORCEINLINE FIntVector FVoreealRegion::GetUpperInt() const
+{ 
+	return FIntVector(X, Y, Z); 
+}
+
+FORCEINLINE FIntVector FVoreealRegion::GetLowerInt() const
+{ 
+	return FIntVector(X + Width, Y + Height, Z + Depth); 
+}
+
+FORCEINLINE FVector FVoreealRegion::GetCenter() const
+{
+	return FVector(X + (Width / 2), Y + (Height / 2), Z + (Depth / 2));
+}
+
+FORCEINLINE void Grow(int32 Width, int32 Height, int32 Depth, FVoreealRegion& Result)
+{
+	Result = FVoreealRegion();
+	Result.X -= Width;
+	Result.Y -= Height;
+	Result.Z -= Depth;
+	Result.Width += Width * 2;
+	Result.Height += Height * 2;
+	Result.Depth += Depth * 2;
+}
+
+FORCEINLINE void FVoreealRegion::Grow(int32 Width, int32 Height, int32 Depth)
+{
+	X -= Width;
+	Y -= Height;
+	Z -= Depth;
+	Width += Width * 2;
+	Height += Height * 2;
+	Depth += Depth * 2;
+}
+
+FORCEINLINE void FVoreealRegion::GrowUnified(int32 Amount)
+{
+	X -= Amount;
+	Y -= Amount;
+	Z -= Amount;
+	Width += Amount * 2;
+	Height += Amount * 2;
+	Depth += Amount * 2;
+}
+
+FORCEINLINE void FVoreealRegion::GrowUnified(int32 Amount, FVoreealRegion& Result)
+{
+	Result = FVoreealRegion();
+	Result.X -= Amount;
+	Result.Y -= Amount;
+	Result.Z -= Amount;
+	Result.Width += Amount * 2;
+	Result.Height += Amount * 2;
+	Result.Depth += Amount * 2;
+}
+
+FORCEINLINE void FVoreealRegion::ShiftUpperCorner(int32 X, int32 Y, int32 Z)
+{
+	X += X;
+	Y += Y;
+	Z += Z;
+	Width -= X;
+	Height -= Y;
+	Depth -= Z;
+}
+
+FORCEINLINE void FVoreealRegion::ShiftUpperCorner(int32 X, int32 Y, int32 Z)
+{
+	Width += X;
+	Height += Y;
+	Depth += Z;
+}
+
+FORCEINLINE bool FVoreealRegion::Contains(const FVoreealRegion& Region1, const FVoreealRegion& Region2)
+{
+	return (Region2.X < (Region1.X + Region1.Width))
+		&& (Region1.X < (Region2.X + Region2.Width))
+		&& (Region2.Y < (Region1.Y + Region1.Height))
+		&& (Region1.Y < (Region2.Y + Region2.Height))
+		&& (Region2.Z < (Region1.Z + Region1.Depth))
+		&& (Region1.Z < (Region2.Z + Region2.Depth));
+}
+
+FORCEINLINE bool FVoreealRegion::Intersect(const FVoreealRegion& Region1, const FVoreealRegion& Region2)
+{
+	// TODO: Intersect
+	return (Region2.X < (Region1.X + Region1.Width))
+		&& (Region1.X < (Region2.X + Region2.Width))
+		&& (Region2.Y < (Region1.Y + Region1.Height))
+		&& (Region1.Y < (Region2.Y + Region2.Height))
+		&& (Region2.Z < (Region1.Z + Region1.Depth))
+		&& (Region1.Z < (Region2.Z + Region2.Depth));
+}
